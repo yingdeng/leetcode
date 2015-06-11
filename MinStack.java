5907130d7d2406442a294faf17ed8c34e250df25
Ying <xiaodengali@gmail.com>
2015-06-11 16:48:55 -0400

    Update MinStack.java
    Stack class supports top operation(peek()).
    If the stack is empty, stack class will throw an exception, we don't need to handle that

--- MinStack.java
+++ MinStack.java
@@ -4,12 +4,13 @@ class MinStack {
     
     public void push(int x) {
         stack.add(x);
-        if (minStack.isEmpty() || minStack.get(minStack.size() - 1) >= x) {
+        if (minStack.isEmpty() || minStack.peek() >= x) {
             minStack.add(x);
         }
     }
 
     public void pop() {
+        /*
         if (stack.isEmpty()) {
             return;
         }
@@ -17,19 +18,33 @@ class MinStack {
         if (!minStack.isEmpty() && a == minStack.get(minStack.size()-1)) {
             minStack.remove(minStack.size()-1);
         }
+        */
+        
+        //redo
+        int val = stack.pop();
++       if (val == minStack.peek()) {
++            minStack.pop();
++       }
++       return val;
     }
 
     public int top() {
+        /*
         if (!stack.isEmpty()) {
             return stack.get(stack.size() - 1);
         }
         return 0;
+        */
+        return stack.pop();
     }
 
     public int getMin() {
+        /*
         if (!minStack.isEmpty()) {
             return minStack.get(minStack.size() - 1);
         }
         return 0;
+        */
+        return minStack.pop();
     }
 }
