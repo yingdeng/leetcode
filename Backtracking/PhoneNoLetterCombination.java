9b119125b63eb078917491e71f8cf44d4a0686e2
Ying <xiaodengali@gmail.com>
2015-06-19 16:51:09 -0400

    Update PhoneNoLetterCombination.java

--- Backtracking/PhoneNoLetterCombination.java
+++ Backtracking/PhoneNoLetterCombination.java
@@ -1,6 +1,17 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+
 public class Solution {
-    public ArrayList<String> letterCombinations(String digits) {
-        ArrayList<String> result = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
         if (digits == null) {
             return result;
         }
@@ -12,33 +23,17 @@ public class Solution {
         return result;
     }
    
-    public void helper(String digits, int index, StringBuilder sb, String[] keyboard, ArrayList<String> result) {
+    public void helper(String digits, int index, StringBuilder sb, String[] keyboard, List<String> result) {
         if (index == digits.length()) {
             result.add(sb.toString());
             return;
         }
         
-        int num = digits.charAt(index)-'0';
+        int num = digits.charAt(index) - '0';
         for (int i = 0; i < keyboard[num].length(); i++) {
             sb.append(keyboard[num].charAt(i));
             helper(digits, index+1, sb, keyboard, result);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
-
-/*
-private void helper(String digits, int index, StringBuilder sb, String[] keyboard, ArrayList<String> result){
-      if (index==digits.length()){
-         result.add(sb.toString());
-         return;
-         }
-         
-       int num=digits.charAt(index)-'0';//get integer number
-       for (int i=0; i<keyboard[num].length(); i++){
-         sb.append(keyboard[num].charAt(i));
-         helper(digits, index+1, current, keyboard, result);
-         sb.deleteCharAt(current.length()-1);
-         }
-     }
-*/
 }
