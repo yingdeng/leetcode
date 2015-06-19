e2a3477cc4a7bf4c3adff9ee57d8e452313d72ca
Ying <xiaodengali@gmail.com>
2015-06-19 17:15:22 -0400

    Update Subsets.java

--- Backtracking/Subsets.java
+++ Backtracking/Subsets.java
@@ -1,25 +1,42 @@
+/*
+Subsets
+Given a set of distinct integers, nums, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If nums = [1,2,3], a solution is:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
 public class Solution {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        
-        if (S == null || S.length == 0){
-            return result;
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 0 || nums == null) {
+            return res;
         }
-        
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        Arrays.sort(S);
-        subsetsHelper(result, list, S, 0);
-        return result;
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        helper(res, list, nums, 0);
+        return res;
     }
     
-    private void subsetsHelper(ArrayList<ArrayList<Integer>> result,
-        ArrayList<Integer> list, int[] S, int pos){
-            result.add(new ArrayList<Integer>(list));
-            for(int i=pos; i<S.length; i++){
-                list.add(S[i]);
-                subsetsHelper(result, list, S, i+1);
-                list.remove(list.size()-1);
-            }
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int pos) {
+        res.add(new ArrayList<Integer>(list));
+        for (int i = pos; i < nums.length; i++) {
+            list.add(nums[i]);
+            helper(res, list, nums, i+1);
+            list.remove(list.size() - 1);
         }
+    }
 }
