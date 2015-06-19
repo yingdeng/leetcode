1974f16686c06c9efbbacebb82c89bf644e1560c
Ying <xiaodengali@gmail.com>
2015-06-19 16:54:46 -0400

    Update CombinationSumII.java

--- Backtracking/CombinationSumII.java
+++ Backtracking/CombinationSumII.java
@@ -1,29 +1,45 @@
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+*/
+
 public class Solution {
-    public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();  
-        if(num == null || num.length==0) {
+    public List<ArrayList<Integer>> combinationSum2(int[] num, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        if(num == null || num.length == 0) {
               return res;  
         } 
         Arrays.sort(num); 
-        helper(num,0,target,new ArrayList<Integer>(),res);  
+        helper(num, 0, target, tempList, res);  
         return res;  
     }
     
-    private void helper(int[] num, int start, int target, ArrayList<Integer> item,  
-        ArrayList<ArrayList<Integer>> res) {
+    private void helper(int[] num, int start, int target, List<Integer> tempList,  
+        List<List<Integer>> res) {
             if(target == 0){
-                res.add(new ArrayList<Integer>(item));
+                res.add(new ArrayList<Integer>(tempList));
                 return;
             }
             if(target < 0){
                 return;
             }
-            for(int i=start; i<num.length; i++){
-                if(i > start && num[i] == num[i-1]){
+            for(int i = start; i < num.length; i++){
+                if(i > start && num[i] == num[i - 1]){
                     continue;
                 }
-                item.add(num[i]);
-                helper(num, i+1, target-num[i], item, res);
+                tempList.add(num[i]);
+                int newTarget = target - num[i];
+                helper(num, i+1, newTarget, tempList, res);
                 item.remove(item.size() - 1);
             }
         }
