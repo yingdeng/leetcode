d32343b25ee94171b80f045e3bb5c260ce888025
Ying <xiaodengali@gmail.com>
2015-06-22 22:38:04 -0400

    Update ClimbingStairs.java

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -1,3 +1,7 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
 public class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1 || n == 2) {
