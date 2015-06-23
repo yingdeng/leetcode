5ac0ac904719735e33155d70c97ba9b7ec7a3795
Ying <xiaodengali@gmail.com>
2015-06-22 22:32:38 -0400

    Update DecodeWays.java

--- DP/DecodeWays.java
+++ DP/DecodeWays.java
@@ -1,3 +1,19 @@
+/*
+Decode Ways - 
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+The number of ways decoding "12" is 2.
+*/
+
 public class Solution {
     public int numDecodings(String s) {
         if (s == null || s.length() == 0) {
@@ -18,11 +34,11 @@ public class Solution {
         
         // Two numbers of s
         for (int i = 2; i <= s.length(); i++) {
-            if (isValid(s.substring(i-1, i))) {
-                res[i] += res[i-1];
+            if (isValid(s.substring(i - 1, i))) {
+                res[i] += res[i - 1];
             }
-            if (isValid(s.substring(i-2, i))) {
-                res[i] += res[i-2];
+            if (isValid(s.substring(i - 2, i))) {
+                res[i] += res[i - 2];
             }
         }
         
