e7319206b4dc6fd3fe7eb023b9ee267e18d3a7c7
Ying <xiaodengali@gmail.com>
2015-06-22 22:17:45 -0400

    Update WorkBreak.java

--- DP/WorkBreak.java
+++ DP/WorkBreak.java
@@ -1,3 +1,18 @@
+/*
+Work Break
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+
+The key to solve this problem by using dynamic programming approach:
+1. Define an array t[] such that t[i]==true => 0-(i-1) can be segmented using dictionary
+2. Initial state t[0] == true
+*/
+
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
         if (s.length() == 0 || s == null) {
@@ -9,7 +24,7 @@ public class Solution {
             StringBuilder str = new StringBuilder(s.substring(0, i+1));
             for (int j = 0; j <= i; j++) {
                 if (res[j] && dict.contains(str.toString())) {
-                    res[i+1] = true;
+                    res[i + 1] = true;
                     break;
                 }
                 str.deleteCharAt(0);
