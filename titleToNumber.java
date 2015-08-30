ef764195a37468be4b45553319df880caeae52d7
Ying <xiaodengali@gmail.com>
2015-08-30 11:12:37 -0400

    titleToNumber

---
+++ titleToNumber.java
@@ -0,0 +1,56 @@
+/*
+Excel Sheet Column Number 
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+*/
+
+public int titleToNumber(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('A', 1);
+        map.put('B', 2);
+        map.put('C', 3);
+        map.put('D', 4);
+        map.put('E', 5);
+        map.put('F', 6);
+        map.put('G', 7);
+        map.put('H', 8);
+        map.put('I', 9);
+        map.put('J', 10);
+        map.put('K', 11);
+        map.put('L', 12);
+        map.put('M', 13);
+        map.put('N', 14);
+        map.put('O', 15);
+        map.put('P', 16);
+        map.put('Q', 17);
+        map.put('R', 18);
+        map.put('S', 19);
+        map.put('T', 20);
+        map.put('U', 21);
+        map.put('V', 22);
+        map.put('W', 23);
+        map.put('X', 24);
+        map.put('Y', 25);
+        map.put('Z', 26);
+        
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res = res*26 + map.get(s.charAt(i));
+        }
+        return res;
+    }
